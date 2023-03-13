3c76e1ced53e3b31f1ae5aa83acbee71bf12ebac
Glenn White <Glennwhite5@gmail.com>
2023-03-13 13:43:34 -0400

    Added a optIn for the screen to run

--- app/src/main/java/com/example/unsplashdemo2/screens/search/SearchScreen.kt
+++ app/src/main/java/com/example/unsplashdemo2/screens/search/SearchScreen.kt
@@ -6,11 +6,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import androidx.paging.ExperimentalPagingApi
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.annotation.ExperimentalCoilApi
 import com.example.unsplashdemo2.navigation.common.ListContent
 
-@OptIn(ExperimentalCoilApi::class)
+@OptIn(ExperimentalCoilApi::class, ExperimentalPagingApi::class)
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun SearchScreen(
